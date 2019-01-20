1cd05dc4010a3073e1178a399e9fd6b493073ada
AranyaAryaman <39076870+AranyaAryaman@users.noreply.github.com>
2019-01-20 11:47:41 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(38, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(204, 22)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(38, 100)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(204, 22)
+        Me.TextBox2.TabIndex = 1
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(68, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter your Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(68, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(139, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter your Password"
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button1.Location = New System.Drawing.Point(38, 128)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(204, 26)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Check Strength"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(38, 160)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(204, 22)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 215)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 26)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Refresh"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(93, 215)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(80, 26)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(179, 215)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(91, 26)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Message"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Transparent
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_BorderStyleChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click        'On Clicking the check button in the form through a mouse, the following things happen 
+        Dim username As String = TextBox1.Text                ' defining username as a string as the content of TextBox1
+        Dim password As String = TextBox2.Text              ' defining password as a string as the content of TextBox2 
+        Dim txtout As String                                'Defining txtout as a string which will be used in future as the output in TextBox3 after clicking Check
+        Dim var As Integer                        ' Defining Var as Integer
+        Dim score As Integer = 0                     ' Similarly defining other variables like score,length
+        Dim length As Integer
+        Dim FinalScore As Decimal = 0
+        Dim lowercount As Integer = 0                        ' initialising the count of different possible
+        Dim uppercount As Integer = 0                        ' allowed characters in the password to 0
+        Dim intcount As Integer = 0
+        Dim charcount As Integer = 0
+        txtout = ""
+        length = password.Length                         ' length is defined as length of string stored in variable password
+        If username.Length = 0 Then             ' if username is not declared an Error Message is shown
+            MessageBox.Show("The Username can't be empty", "Error")
+            FinalScore = -120                        ' the score is set to -120 
+        End If
+        If length < 8 Then                                     ' If password length is less than 8, attention message is shown as smaller passwords are usually weaker 
+            MessageBox.Show("The Password is smaller than 8 letters", "Attention")
+        ElseIf length >= 8 And String.Compare("PASSWORD", password, True) = 0 Then
+            FinalScore = -40                                                 ' If "password"(Case Ignored) is set as password, it is a weaker password
+            MessageBox.Show("You have set your password as :- " & password, "Error")
+        ElseIf String.Compare(username, password, True) = 0 Then              ' If the entered username is set as password, it is again a weak password
+            FinalScore = -40
+            MessageBox.Show("You are setting your Username as your Password", "Attention") ' Message Box is displayed
+        End If
+        For var = 0 To length - 1           ' Iterating through the string stored inside password variable
+            If CInt(AscW(password(var))) >= 97 And CInt(AscW(password(var))) <= 122 Then      ' ASCII of lowercase alphabets
+                lowercount += 1       ' incrementing counts
+            ElseIf CInt(AscW(password(var))) >= 65 And CInt(AscW(password(var))) <= 90 Then ' ASCII of Uppercase alphabets
+                uppercount += 1
+            ElseIf CInt(AscW(password(var))) >= 48 And CInt(AscW(password(var))) <= 57 Then        ' ASCII of Integers
+                intcount += 1
+            ElseIf CInt(AscW(password(var))) >= 33 And CInt(AscW(password(var))) <= 47 Then         ' Rest all are ASCII of Characters
+                charcount += 1
+            ElseIf CInt(AscW(password(var))) >= 58 And CInt(AscW(password(var))) <= 64 Then
+                charcount += 1
+            ElseIf CInt(AscW(password(var))) >= 91 And CInt(AscW(password(var))) <= 96 Then
+                charcount += 1
+            ElseIf CInt(AscW(password(var))) >= 123 And CInt(AscW(password(var))) <= 127 Then
+                charcount += 1
+            ElseIf CInt(AscW(password(var))) = 32 Then               ' If space is a character of the password, the password is not accepted
+                MessageBox.Show("You have entered a space character in Password", "Error")
+                FinalScore = -120          ' A score of -120 is alloted so that the password becomes Invalid
+                Exit For
+            Else
+                MessageBox.Show("Unknown Error occured", "Error")       ' Outside lowercase, uppercase,integers or special characters in the above range, any other character is not allowed
+                FinalScore = -120             ' So a score of -120 is alloted so that the password becomes Invalid
+                Exit For
+            End If
+        Next
+        If length >= 8 Then
+            FinalScore += 20                ' If length is greater than 8 , score of 20 is alloted
+        End If
+        If lowercount >= 1 Then                ' lowercase characters are weaker , so 15 score is given
+            FinalScore += 15
+        End If
+        If charcount >= 1 Then
+            FinalScore += 23                   ' special characters are tougher to guess, so a final score of 23 is added
+        End If
+        If intcount >= 1 Then
+            FinalScore += 22               ' similarly for integers and uppercase characters
+        End If
+        If uppercount >= 1 Then
+            FinalScore += 20
+        End If
+        If CInt(FinalScore) > 80 Then                        ' Finally Judging the Password Strength in b/w the specified range
+            txtout &= "Very Strong"
+        ElseIf CInt(FinalScore) > 65 And CInt(FinalScore) <= 80 Then
+            txtout &= "Strong"
+        ElseIf CInt(FinalScore) > 45 And CInt(FinalScore) <= 65 Then
+            txtout &= "Good"
+        ElseIf CInt(FinalScore) > 25 And CInt(FinalScore) <= 45 Then
+            txtout &= "Weak"                  ' "&" is used for concatenating strings
+        ElseIf CInt(FinalScore) >= 0 And CInt(FinalScore) <= 25 Then
+            txtout &= "Very Weak"
+        Else                                        ' For cases like password having a space(" ") in the string or any character outside the specified range of ASCII is entered , the password becomes invalid 
+            txtout &= "Invalid"
+        End If
+        TextBox3.Text = txtout     ' finally printing the content of txtout in TextBox3
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Restart()                     ' If Restart Button is Clicked , the application restarts 
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()           ' If Exit Button is clicked, the application closes 
+    End Sub
+End Class
